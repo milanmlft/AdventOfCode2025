34c427c7dec7510976fdea7430f304971b9c9747
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-12-24 15:23:55 +0100

    Day 4 part 2

---
+++ day4/src/part2.rs
@@ -0,0 +1,96 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Debug)]
+struct Grid {
+    cells: Vec<Vec<char>>,
+    nrow: usize,
+    ncol: usize,
+}
+
+impl Grid {
+    fn count_neighbours(&self, x: usize, y: usize) -> u32 {
+        let mut n = 0;
+
+        let nrow = self.nrow as isize;
+        let ncol = self.ncol as isize;
+
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+
+                let i = x as isize + dx;
+                let j = y as isize + dy;
+
+                if i >= 0
+                    && i < nrow
+                    && j >= 0
+                    && j < ncol
+                    && self.cells[i as usize][j as usize] == '@'
+                {
+                    n += 1;
+                }
+            }
+        }
+        n
+    }
+}
+
+fn solve(filename: &str) -> u32 {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    // Construct grid
+    let mut grid = Grid {
+        cells: Vec::new(),
+        nrow: 0,
+        ncol: 0,
+    };
+
+    for line in reader.lines() {
+        grid.cells.push(line.unwrap().chars().collect());
+        grid.nrow += 1;
+    }
+    grid.ncol = grid.cells[0].len();
+
+    // Remove rolls of paper until we can't
+    let mut result = 0;
+    loop {
+        let mut nremoved = 0;
+        let mut new_cells = grid.cells.clone();
+        for (i, _) in grid.cells.iter().enumerate() {
+            for (j, _) in grid.cells[i].iter().enumerate() {
+                if grid.cells[i][j] == '@' && grid.count_neighbours(i, j) < 4 {
+                    nremoved += 1;
+                    new_cells[i][j] = 'x';
+                }
+            }
+        }
+        if nremoved == 0 {
+            break;
+        }
+        grid.cells = new_cells;
+        result += nremoved;
+    }
+
+    result
+}
+
+fn main() {
+    println!("{}", solve("input"));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn day4_part1_test() {
+        let solution = solve("test_input");
+        assert_eq!(solution, 43);
+    }
+}
